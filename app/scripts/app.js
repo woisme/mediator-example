889ba7d676fadd8de4192461ee46987e0cf248a9
woisme <mywoisme@gmail.com>
2015-10-04 14:13:25 +0100

    delete button always appears and the cards are always elevated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,15 +1,13 @@
-(function(document) {
-  // 'use strict';
-
-  // Grab a reference to our auto-binding template
-  // and give it some initial binding values
-  // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  // var app = document.querySelector('#app');
-
-  // Let the user know that offline caching has worked and their
-  // app is available offline
-  // app.displayInstalledToast = function() {
-  //   document.querySelector('#caching-complete').show();
-  // };
-
-})(document);
+// (function(document) {
+//
+//   // Grab a reference to our auto-binding template
+//   // and give it some initial binding values
+//   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+//
+//   // Let the user know that offline caching has worked and their
+//   // app is available offline
+//    app.displayInstalledToast = function() {
+//      document.querySelector('#caching-complete').show();
+//    };
+//
+// })(document);
